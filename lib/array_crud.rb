c7a0a5b78633a5e3cc0d3ad0c69358167e83ac28
sarah <s.j.condon01@gmail.com>
2019-09-13 20:41:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,15 +7,15 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("Boo")
+  array.push("element")
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshrift("monkey")
+  array.unshrift("element")
 end
 
 def remove_element_from_end_of_array(array)
-  Array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
